6868030bcaead27819086097126e7b45e6600b7a
magalene <magaleneprice@gmail.com>
2024-01-07 15:14:25 -0600

    Homework

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,26 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceMarsKm = 225000000;
+let distanceMoonKm = 384400;
+let mPKm = 0.621;
+
+console.log(typeof(shuttleName));
+console.log(typeof(shuttleSpeedMph));
+console.log(typeof(distanceMarsKm));
+console.log(typeof(distanceMoonKm));
+console.log(typeof(mPKm));
+
+let milesToMars = distanceMarsKm * mPKm;
+let hoursToMars = shuttleSpeedMph * milesToMars;
+let daysToMars = hoursToMars / 24;
+
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars. ");
+
+let milesToMoon = distanceMoonKm + mPKm;
+let hoursToMoon = milesToMoon * shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon");
