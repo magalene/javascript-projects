d2b4bd8d47668e70a73828ca9913438a1dc45008
Magalene <magaleneprice@gmail.com>
2024-02-12 09:36:03 -0600

    studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,55 @@
-
 let launchcode = {
-  
-}
+  organization:"nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees:100,
+  programsOffered:["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(n)
+  {
+      let result = "";
+      if ( n % 2 == 0)
+      {
+          if (n %3==0)
+          {
+              if ( n %5 == 0)
+              {
+                  result = "LaunchCode Rocks!";
+              }
+              else
+              {
+                  result = "LaunchCode!";
+              }
+          }
+          else if ( n % 5 ==0)
+          {
+              result = "Launch Rocks! (CRASH!!!!)";
+          }
+          else
+          {
+              result = "Launch!";
+          }
+
+      }
+      else if ( n % 3 == 0 )
+      {
+          if (n %5==0)
+          {
+              result = "Code Rocks!";
+          }
+          else
+          {
+              result = "Code!";
+          }
+      }
+      else if ( n % 5 == 0 )
+      {
+          result = "Rocks!";
+      }
+      else
+      {
+          result = "Rutabagas! That doesn't work.";
+      }
+      return result;
+  }
+};
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,13 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("Return 'nonprofit' as key value of organization",function(){
+   let keyVal = launchcode.organization;
+    expect(keyVal).toBe('nonprofit');
+  });
+  test("returns 'Launch!' when divisible by 2", function(){
+    // let equation = n%2 === 0
+    expect(launchcode.launchOutput(2)).toBe('Launch!')
+  });
+
 });
